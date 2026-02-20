a91f267f734ae6386bc8ef125c4286589ec38d2f
Jose Blanco <blancocasal@hotmail.com>
2026-02-20 15:40:25 +0100

    Solucion final fondos grises version 1.1.4

--- lexhoy-despachos.php
+++ lexhoy-despachos.php
@@ -3,7 +3,7 @@
  * Plugin Name: LexHoy Despachos
  * Plugin URI: https://lexhoy.com
  * Description: Plugin para gestionar despachos de LexHoy
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: LexHoy
  * Author URI: https://lexhoy.com
  * Text Domain: lexhoy-despachos
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Definir constantes
-define('LEXHOY_DESPACHOS_VERSION', '1.1.3');
+define('LEXHOY_DESPACHOS_VERSION', '1.1.4');
 define('LEXHOY_DESPACHOS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('LEXHOY_DESPACHOS_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('LEXHOY_DESPACHOS_PLUGIN_FILE', __FILE__);
